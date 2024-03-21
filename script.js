bb731e7c10c77ffac53a8e7969cead0afcacfc28
LQuesadaM <luisquesada163@gmail.com>
2024-03-21 08:06:12 -0300

    🍻 getting the values of values-dropdown in an array

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const sortButton = document.getElementById("sort");
 // prevent reload page
 const sortInputArray = (e) => {
   event.preventDefault();
+  const inputValues = [...document.getElementsByClassName("values-dropdown")];
 };
 
 sortButton.addEventListener("click", sortInputArray);
