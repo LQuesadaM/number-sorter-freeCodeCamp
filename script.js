0bb4c2788fa3dc9f5c8956183e9d3e762292773c
LQuesadaM <luisquesada163@gmail.com>
2024-03-21 08:17:17 -0300

    🍻 getting the values of the selection that are in str and transformed to numbers.

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const sortButton = document.getElementById("sort");
 // prevent reload page
 const sortInputArray = (e) => {
   event.preventDefault();
-  const inputValues = [...document.getElementsByClassName("values-dropdown")];
+  const inputValues = [
+    ...document.getElementsByClassName("values-dropdown"),
+  ].map((dropdown) => Number(dropdown.value));
+  console.log(inputValues);
 };
 
 sortButton.addEventListener("click", sortInputArray);
